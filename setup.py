efcc03ebcb005a41640eb206be007330d6b0a897
Bird <mostafa.shaeri.tj@gmail.com>
2023-03-25 05:14:30 -0700

    Remove zip_safe

--- setup.py
+++ setup.py
@@ -6,12 +6,11 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='django-query-to-table',
-    version='0.1.5',
+    version='0.1.6',
     description='A simple to use Django package to turn your sql query into a beautiful reporting html table',
     long_description_content_type="text/markdown",
     long_description=README,
     license='GNU',
-    zip_safe=True,
     packages=find_packages(),
     author='M.Shaeri',
     
