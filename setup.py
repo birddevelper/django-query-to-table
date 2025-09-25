0e63d69cbf0f12fee82fb3475204e6f11f35077f
Bird <mostafa_shaeri_tj@yahoo.com>
2025-09-25 03:42:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='django-query-to-table',
-    version='1.1.0',
+    version='1.1.1',
     description='A simple to use Django package to turn your queryset and sql query into a beautiful reporting html table',
     long_description_content_type="text/markdown",
     long_description=README,
     license='GNU',
     packages=find_packages(),
-    author='M.Shaeri',
+    author='R.E',
     
     keywords=['Django', 'Report', 'HTML', 'Table', 'SQL'],
     url='https://github.com/birddevelper/django-query-to-table',
